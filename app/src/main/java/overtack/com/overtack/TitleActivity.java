b6f6d3b2ff204a2042098cbba37880f511904f0d
crazyma-nn <igh001bim0@gmail.com>
2018-06-17 23:02:52 +0900

    タイトル画面にはい・いいえボタンの実装

--- app/src/main/java/overtack/com/overtack/TitleActivity.java
+++ app/src/main/java/overtack/com/overtack/TitleActivity.java
@@ -1,24 +1,43 @@
 package overtack.com.overtack;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.Fragment;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.os.Bundle;
+import android.widget.Button;
 
-public class TitleActivity extends AppCompatActivity implements View.OnClickListener{
+public class TitleActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_title);
 
-        findViewById(R.id.button).setOnClickListener(this);
+        Button button1 = (Button)findViewById(R.id.button);
+        button1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity (intent);
+            }
+        });
+
+        Button button2 = (Button)findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+                new AlertDialog.Builder(TitleActivity.this)
+                .setTitle("テスト")
+                .show();
+
+            }
+        });
+
     }
 
-
-    //ボタンが押された時の処理
-    public void onClick(View view){
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity (intent);
-    }
 }
